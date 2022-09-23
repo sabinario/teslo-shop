f4a026d7f7a5f2ae47116754cabc9314f806d271
Sabino Fernandez <sabinofernandezr@gmail.com>
2022-09-23 16:56:16 -0500

    react hook form integration

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,53 +1,73 @@
 import React from 'react';
 
 import NextLink from 'next/link';
+import { useForm } from 'react-hook-form';
+import { SubmitHandler } from 'react-hook-form/dist/types';
 
 import { AuthLayout } from '../../components/layouts';
 import { Box, Button, Grid, Link, TextField, Typography } from '../../shared';
 
+type FormData = {
+	email: string;
+	password: string;
+};
+
 const LoginPage = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>();
+
+	const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
+
 	return (
 		<AuthLayout title='Ingresar'>
-			<Box sx={{ width: 350, padding: '10px 20px' }}>
-				<Grid container spacing={2}>
-					<Grid xs={12}>
-						<Typography variant='h1' component='h1'>
-							Iniciar sesión
-						</Typography>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<Box sx={{ width: 350, padding: '10px 20px' }}>
+					<Grid container spacing={2}>
+						<Grid xs={12}>
+							<Typography variant='h1' component='h1'>
+								Iniciar sesión
+							</Typography>
+						</Grid>
+						<Grid xs={12}>
+							<TextField
+								label='Correo'
+								variant='outlined'
+								fullWidth
+								type='email'
+								{...register('email')}
+							/>
+						</Grid>
+						<Grid xs={12}>
+							<TextField
+								label='Contraseña'
+								type='password'
+								variant='outlined'
+								fullWidth
+								{...register('password')}
+							/>
+						</Grid>
+						<Grid xs={12}>
+							<Button
+								color='secondary'
+								className='circular-btn'
+								size='large'
+								fullWidth
+								type='submit'
+							>
+								Ingresar
+							</Button>
+						</Grid>
+						<Grid xs={12} display='flex' justifyContent='end'>
+							<NextLink href='/auth/register' passHref>
+								<Link underline='always'>¿No tienes cuenta?</Link>
+							</NextLink>
+						</Grid>
 					</Grid>
-					<Grid xs={12}>
-						<TextField
-							label='Correo'
-							variant='outlined'
-							fullWidth
-							type='email'
-						/>
-					</Grid>
-					<Grid xs={12}>
-						<TextField
-							label='Contraseña'
-							type='password'
-							variant='outlined'
-							fullWidth
-						/>
-					</Grid>
-					<Grid xs={12}>
-						<Button
-							color='secondary'
-							className='circular-btn'
-							size='large'
-							fullWidth
-						>
-							Ingresar
-						</Button>
-					</Grid>
-					<Grid xs={12} display='flex' justifyContent='end'>
-						<NextLink href='/auth/register' passHref>
-							<Link underline='always'>¿No tienes cuenta?</Link>
-						</NextLink>
-					</Grid>
-				</Grid>
-			</Box>
+				</Box>
+			</form>
 		</AuthLayout>
 	);
 };
